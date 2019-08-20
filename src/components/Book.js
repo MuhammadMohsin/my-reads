60bc9acc675233fde402264db184c0a8161e84fc
MuhammadMohsin <mmohsinkhalid@outlook.com>
2019-08-20 22:04:36 +0500

    Add Book component and integrate API serivce to get all books

---
+++ src/components/Book.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Book = (props) => {
+    const { books } = props;
+
+    return (
+        <ol className="books-grid">
+            {books.length ?
+                books.map(book => {
+                    return (<li key={book.id}>
+                        <div className="read">
+                            <div className="book-top">
+                                <div className="book-cover" style={{ backgroundImage: 'url(' + book.imageLinks.smallThumbnail + ')' }}></div>
+                                <div className="book-shelf-changer">
+                                    <select>
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.authors}</div>
+                        </div>
+                    </li>)
+                })
+                : <h3>You do not have any book in this shelf!</h3>}
+        </ol>
+
+    )
+}
+
+export default Book;
